642678fa42cd588b1a4b4b7e136b145dac7d2c7f
09shraddhagoudar <shraddhakrishnagoudar@gmail.com>
2024-02-22 19:27:00 +0530

    changes

--- src/views/Services/Services.js
+++ src/views/Services/Services.js
@@ -1,15 +1,35 @@
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
 
+import '../Services.css';
+import { Services, HEAD_TITLE } from './../Services/config';
 
-function Services() {
+
+function Services () {
   return (
-    <div>
-        <Navbar />
-        <h1 style={{textAlign: 'center',margin: '130px',fontSize: '70px'}}> Services 🏡 </h1>
-        <Footer />
-    </div>
+    <>
+  <section className='services-main-container'>
+        <div className='container service-container'>
+        <h1 className='main-heading text-center fw-bold'>Our Services</h1>
+            <div className="row">
+            { serviceData.map((curElem)=>{
+              const {id,logo,title,info}=curElem;
+              return(
+              <>
+               <div className="col-11 col-lg-4 col-xxl-4 work-container-subdiv" 
+               key={id}>
+<i className={`fontawesome-style ${logo}`}></i>
+<h2 className="sub-heading">{title}</h2>
+<p>{info} </p>
+            </div>
+              </>);
+            })}
+               </div>
+      </div>
+     </section>
+    </>
   )
 }
+
 
 export default Services
---
+++ src/views/Services/config.js
@@ -0,0 +1,36 @@
+const serviceapi =[
+    {
+        id:1,
+        logo:"fas fa-download",
+        title:"Register for free",
+        info:"hagxah",
+
+    },
+
+    {
+        id:2,
+        logo:"fas fa-user-check",
+        title:"Register for free",
+        info:"hagxah",
+
+    },
+    {
+        id:3,
+        logo:"fas fa-download",
+        title:"Register for free",
+        info:"hagxah",
+
+    },
+    {
+        id:4,
+        logo:"fas fa-download",
+        title:"Register for free",
+        info:"hagxah",
+
+    },
+]
+const HEAD_TITLE = "Our Services"
+export{
+    Services,
+    HEAD_TITLE
+}
