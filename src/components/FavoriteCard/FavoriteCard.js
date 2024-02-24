a99113cb04458d1e01c1fb362e72ba2cedbe2494
harshaal06 <harshalaglawe06@gmail.com>
2024-02-24 15:04:10 +0530

    some change

--- src/components/FavoriteCard/FavoriteCard.js
+++ src/components/FavoriteCard/FavoriteCard.js
@@ -8,7 +8,7 @@ function FavoriteCard({imge,title,description,link}) {
         <img src={imge} className='w-100 border'/>
         <div className='container'>
             <p className='fw-semibold my-2'>{title.toUpperCase()}</p>
-            <p className='fw-light fst-italic lh-sm'>{description.substring(0,25)}...<Link to={link} className='text-decoration-none fw-medium fst-italic m-2'>Read More</Link></p>
+            <p className='fw-light fst-italic lh-sm'>{description.substring(0,19)}...<Link to={link} className='text-decoration-none fw-medium fst-italic m-2'>Read More</Link></p>
             <hr className='m-0'/>
             <p className='text-center m-1 mb-3'><Link to={link}>Remove</Link></p>
         </div>
