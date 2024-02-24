24e7904f9ba9ac04258b73e5ec84dab38be6cb23
missrutu22 <rutujagavhane2003@gmail.com>
2024-02-24 11:33:41 +0530

    contact page

--- src/views/Contact/Contact.js
+++ src/views/Contact/Contact.js
@@ -9,19 +9,24 @@ function Contact() {
   return (
     <div>
         <Navbar />
+
         <h1 className="contact-heading">Contact Us</h1>
+
         <div className="contact-card">
         <img src={Image1} alt="telephone" className='contact-img'/>
-        <p className='location'>+91 8010564312</p>
+        <li className="location">📱<a href="mob:8010564343" className="text-decoration-none">+91 8010564343</a></li>
         </div>
+
         <div className="contact-card">
         <img src={Image2} alt="email" className='contact-img'/>
         <p className='location'>fortresstour@gmail.com</p>
         </div>
+
         <div className="contact-card">
         <img src={Image3} alt="telephone" className='contact-img'/>
-        <p className='location'>84, Fortress Tours,Laksmi narayan chowk ,opposite narayan residency, ward no 3 , prasad colony, Nagpur-441108</p>
+        <p className='location'>84, ,Laksmi narayan chowk ,opposite narayan residency, ward no 3 , prasad colony, Nagpur-441108</p>
         </div>
+
         <Footer />
     </div>
   )
